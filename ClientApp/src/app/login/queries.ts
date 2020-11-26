b79d48721e931bee2c8ac5b16bd82473599b00d0
Pabl021 <josepablobc21@gmail.com>
2020-11-26 09:55:57 -0600

    add  token to login

--- ClientApp/src/app/login/queries.ts
+++ ClientApp/src/app/login/queries.ts
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 export const USERS_QUERY = gql`
 query($email:String, $password:String){
-    users(email:$email, password:$password){
+    login(email:$email, password:$password){
       id
       email
       password
