122939c214d031b4dd4aee41c7eddf9edc347379
Pabl021 <josepablobc21@gmail.com>
2020-11-29 19:14:56 -0600

    changes in mainadmin

---
+++ Migrations/20201128191751_InitialCreate.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace RedCrossBingo.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    email = table.Column<string>(nullable: true),
+                    password = table.Column<string>(nullable: true),
+                    token = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "rooms",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(nullable: true),
+                    url = table.Column<string>(nullable: true),
+                    users_id = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_rooms", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_rooms_users_users_id",
+                        column: x => x.users_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "bingo_cards",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    rooms_id = table.Column<long>(nullable: false),
+                    is_playing = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_bingo_cards", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_bingo_cards_rooms_rooms_id",
+                        column: x => x.rooms_id,
+                        principalTable: "rooms",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "bingo_numbers",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    number = table.Column<long>(nullable: false),
+                    is_chosen = table.Column<bool>(nullable: false),
+                    rooms_id = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_bingo_numbers", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_bingo_numbers_rooms_rooms_id",
+                        column: x => x.rooms_id,
+                        principalTable: "rooms",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "bingo_card_numbers",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    number = table.Column<long>(nullable: false),
+                    is_selected = table.Column<bool>(nullable: false),
+                    bingo_cards_id = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_bingo_card_numbers", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_bingo_card_numbers_bingo_cards_bingo_cards_id",
+                        column: x => x.bingo_cards_id,
+                        principalTable: "bingo_cards",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bingo_card_numbers_bingo_cards_id",
+                table: "bingo_card_numbers",
+                column: "bingo_cards_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bingo_cards_rooms_id",
+                table: "bingo_cards",
+                column: "rooms_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bingo_numbers_rooms_id",
+                table: "bingo_numbers",
+                column: "rooms_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_rooms_users_id",
+                table: "rooms",
+                column: "users_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "bingo_card_numbers");
+
+            migrationBuilder.DropTable(
+                name: "bingo_numbers");
+
+            migrationBuilder.DropTable(
+                name: "bingo_cards");
+
+            migrationBuilder.DropTable(
+                name: "rooms");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
