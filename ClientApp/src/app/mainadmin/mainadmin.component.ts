d8dc11eefa2ad44dfbbc16162416caa041420d2e
Gerardo <gera2espi1818@gmail.com>
2020-12-01 08:07:01 -0600

    save url in db

--- ClientApp/src/app/mainadmin/mainadmin.component.ts
+++ ClientApp/src/app/mainadmin/mainadmin.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component , Inject} from '@angular/core';
 import {ROOMS_QUERY} from './queries';
 import {Rooms} from './mainadmin.interface'
 import {Apollo} from 'apollo-angular';
-import {DELETE_ROOMS} from './mutations'
+import {CREATE_ROOM, DELETE_ROOMS} from './mutations'
 import swal from 'sweetalert';
+
+import {Login} from './../login/login.interface';
 
 @Component({
   selector: 'app-mainadmin',
@@ -15,10 +16,69 @@ import swal from 'sweetalert';
 
 export class MainadminComponent {
   rooms: Rooms[];
-
-  constructor(private apollo: Apollo) {
+  private room: Rooms; 
+  private error = '';
+  private userLogueado : Login; 
+  constructor(private apollo: Apollo,  @Inject('BASE_URL') public baseUrl: string) {
     this.getRooms();
+    this.newRoom(); 
+    this.userInSession(); 
    }
+
+  userInSession(){
+     var user = JSON.parse(sessionStorage.getItem('user')); 
+     this.userLogueado = {
+      id : user['id'],
+      email: user['email'],
+      password: user['password']
+     }
+   }
+
+newRoom(){
+  this.room = {
+    id: 0, 
+    name: '',
+    url: ''    
+  }
+}
+
+createRoom(){
+  
+  try {
+    this.validateRoom(); 
+    this.save(); 
+  } catch (error) {
+    this.error = error.message;
+    setTimeout(()=> {
+      this.error = ''; 
+    }, 3000);
+  }
+}
+
+save(){
+   var newUrl = this.baseUrl+this.room.name; 
+  const variables = {
+    input: {name: this.room.name, url: newUrl, usersId:this.userLogueado.id} 
+  };
+  this.apollo.mutate({
+    mutation : CREATE_ROOM,
+    variables: variables
+  }).subscribe(result=>{
+    console.log(result.data); 
+    this.getRooms(); 
+  }); 
+}
+
+
+validateRoom(){
+  this.error = ''; 
+  var name = this.room.name.toLowerCase().trim().replace(/ /g, "");
+  if(name== ''){
+    throw new Error("You must write name of the room!");
+  }
+
+  this.room.name = name; 
+}
 
  getRooms(){
   this.apollo.watchQuery({
@@ -31,6 +91,7 @@ export class MainadminComponent {
   })
  }
 
+
  deleteRoom(room:Rooms){
   swal({
     title: "Are you sure?",
@@ -38,7 +99,7 @@ export class MainadminComponent {
     icon: "warning",
     buttons: true,
     dangerMode: true,
-  })
+  })  
   .then((willDelete) => {
     if (willDelete) {
       this.apollo.mutate({
@@ -55,6 +116,8 @@ export class MainadminComponent {
     }
   });
  }
+
+
 
 
 
